cd08056605f04f8b7fc9fd742c68d0373ae780e6
frvnnnnn <163429337+frvnnnnn@users.noreply.github.com>
2024-04-27 06:30:31 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -102,7 +102,7 @@ Pair * searchMap(HashMap * map, char * key) {
 
 Pair * firstMap(HashMap * map) {
     for (int i = 0; i < map->capacity; i++) {
-        if (map->buckets[i] != NULL ) {
+        if (map->buckets[i] != NULL && map->buckets[i]!=-1) {
             map->current = i;
             return map->buckets[i];}
     }
